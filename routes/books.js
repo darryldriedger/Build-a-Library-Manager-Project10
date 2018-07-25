bab95bafdce9d0bdff3a3f4f61d40187510ed8c9
darryldriedger <darryldriedger@gmail.com>
2018-07-25 01:13:57 -0600

    changes to routing for books

--- routes/books.js
+++ routes/books.js
@@ -3,28 +3,10 @@
 var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
-var Loans = require("../models").Loans;
-var Patrons = require("../models").Patrons;
+// var Loans = require("../models").Loans;
+// var Patrons = require("../models").Patrons;
 
 
-/* _____ ____  _  _____    ____  _____ _     ____  _
-  /  __//  _ \/ \/__ __\  /  _ \/  __// \   /  _ \/ \  /|
-  |  \  | | \|| |  / \    | | //|  \  | |   | / \|| |  ||
-  |  /_ | |_/|| |  | |    | |_\\|  /_ | |_/\| \_/|| |/\||
-  \____\\____/\_/  \_/    \____/\____\\____/\____/\_/  \|
-*/
-
-//BOOKS
-//BOOKS
-//BOOKS
-
-/* _____          _
- / ____|        | |
-| |  __    ___  | |_
-| | |_ |  / _ \ | __|
-| |__| | |  __/ | |_
-\_____|  \___|  \__|
-*/
 /* _____          _
  / ____|        | |
 | |  __    ___  | |_
@@ -33,29 +15,28 @@ var Patrons = require("../models").Patrons;
 \_____|  \___|  \__|
 */
 //* GET all Books listings. */
-// router.get('/books', function(req, res, next) {
-//   Book.findAll().then(function(books){
-//     res.render('books',{books: books});
-//   }).catch(function(error){
-//       res.send(500, error);
-//    });
-// });
-//
-// router.get('/new_book', function(req, res, next) {
-//   res.render('new_book');
-// });
-// router.get('/overdue_books', function(req, res, next) {
-//   res.render('overdue_books');
-// });
-// router.get('/checked_books', function(req, res, next) {
-//   res.render('checked_books');
-// });
-// router.get('/book_detail', function(req, res, next) {
-//   console.log("one");
-//   res.render('book_detail');
-// });
-//
-//
+router.get('/', function(req, res, next) {
+  Book.findAll().then(function(books){
+    res.render('books',{books: books});
+  }).catch(function(error){
+      res.send(500, error);
+   });
+});
+
+router.get('/new_book', function(req, res, next) {
+  res.render('new_book');
+});
+router.get('/overdue_books', function(req, res, next) {
+  res.render('overdue_books');
+});
+router.get('/checked_books', function(req, res, next) {
+  res.render('checked_books');
+});
+router.get('/book_detail', function(req, res, next) {
+  // console.log("one");
+  res.render('book_detail');
+});
+
 // //* GET Books listing. */
 // // router.get('/books', function(req, res, next) {
 // //   Book.findAll().then(function(books){
@@ -83,40 +64,29 @@ var Patrons = require("../models").Patrons;
 // //    router.get('/new_book', function(req, res, next) {
 // //      res.render('books/new_book');
 // //    });
-//
-// /*
-//   _____                  _
-//  |  __ \                | |
-//  | |__) |   ___    ___  | |_
-//  |  ___/   / _ \  / __| | __|
-//  | |      | (_) | \__ \ | |_
-//  |_|      \___/  |___/  \__|
-//
-// *//* POST create article. */
-// /*
-//   _____                  _
-//  |  __ \                | |
-//  | |__) |   ___    ___  | |_
-//  |  ___/   / _ \  / __| | __|
-//  | |      | (_) | \__ \ | |_
-//  |_|      \___/  |___/  \__|
-//
-// *//* POST create article. */
-// router.post('/new_book', function(req, res, next) {
-//   Book.create(req.body).then(function(book) {
-//     res.redirect("/books");
-//   }).catch(function(error){
-//       if(error.name === "SequelizeValidationError") {
-//         // res.render("articles/new", {article: Article.build(req.body), errors: error.errors, title: "New Article"})
-//         console.log("error");
-//       } else {
-//         throw error;
-//       }
-//   }).catch(function(error){
-//       res.send(500, error);
-//    });
-// ;});
-//
+
+/*
+  _____                  _
+ |  __ \                | |
+ | |__) |   ___    ___  | |_
+ |  ___/   / _ \  / __| | __|
+ | |      | (_) | \__ \ | |_
+ |_|      \___/  |___/  \__|
+
+*//* POST create article. */
+router.post('/new_book', function(req, res, next) {
+  Book.create(req.body).then(function(book) {
+    res.redirect("/books");
+  }).catch(function(error){
+      if(error.name === "SequelizeValidationError") {
+        console.log("error");
+      } else {
+        throw error;
+      }
+  }).catch(function(error){
+      res.send(500, error);
+   });
+;});
 // // router.post('/books/new_book', function(req, res, next) {
 // //   Book.create(req.body).then(function(book) {
 // //     res.redirect("/books");
@@ -133,32 +103,25 @@ var Patrons = require("../models").Patrons;
 // // ;});
 //
 //
-// /*
-//  _____            _
-// |  __ \          | |
-// | |__) |  _   _  | |_
-// |  ___/  | | | | | __|
-// | |      | |_| | | |_
-// |_|       \__,_|  \__|
-// */
-// /* PUT update book detail. */
-// // router.put("/:id", function(req, res, next){
-// //   Book.findById(req.params.id).then(function(bookDetail){
-// //     return bookDetail.update(req.body);
-// //   }).then(function(bookDetail){
-// //     // console.log("we ran the PUT");
-// //     res.redirect('/books');
-// //   }).catch(function(error){
-// //       if(error.name === "SequelizeValidationError") {
-// //         console.log("there was a huge error");
-// //         // res.render("articles/edit", {article: article, errors: error.errors, title: "Edit Article"})
-// //       } else {
-// //         throw error;
-// //       }
-// //   }).catch(function(error){
-// //       res.send(500, error);
-// //    });
-// // });
+
+/*
+ _____            _
+|  __ \          | |
+| |__) |  _   _  | |_
+|  ___/  | | | | | __|
+| |      | |_| | | |_
+|_|       \__,_|  \__|
+*/
+router.put('/:id', function(req, res, next){
+  Book.findById(req.params.id).then(function(bookDetail){
+    return bookDetail.update(req.body);
+  }).then(function(bookDetail){
+    res.redirect('/books/' + bookDetail.id);
+  }).catch(function(error){
+      console.log("there is a huge 500 error here");
+      res.status(500).send(error);
+   });
+});
 //
 // router.put('/book_detail/:id', function(req, res, next){
 //   Book.findById(req.params.id).then(function(book){
@@ -172,28 +135,25 @@ var Patrons = require("../models").Patrons;
 //       res.send(500, error);
 //    });
 // });
-//
-// // /*
-// //  _____          _       _____               _   _
-// // / ____|        | |     |_   _|             | | (_)
-// // | |  __    ___  | |_      | |    _ __     __| |  _  __   __
-// // | | |_ |  / _ \ | __|     | |   | '_ \   / _` | | | \ \ / /
-// // | |__| | |  __/ | |_     _| |_  | | | | | (_| | | |  \ V /
-// // \_____|  \___|  \__|   |_____| |_| |_|  \__,_| |_|   \_/
-// // */
-//
-// // GET incividual book details
-//
-// //------> this findall for book detail was replaced by the find by id below
-// // router.get('/book_detail/:id', function(req, res, next) {
-// //   Book.findAll().then(function(bookDetail){
-// //     console.log("this is the book detail title");
-// //     console.log(bookDetail[6]);
-// //     res.render('book_detail',{bookDetail: bookDetail});
-// //   }).catch(function(error){
-// //       res.send(500, error);
-// //    });
-// // });
+
+// /*
+//  _____          _       _____               _   _
+// / ____|        | |     |_   _|             | | (_)
+// | |  __    ___  | |_      | |    _ __     __| |  _  __   __
+// | | |_ |  / _ \ | __|     | |   | '_ \   / _` | | | \ \ / /
+// | |__| | |  __/ | |_     _| |_  | | | | | (_| | | |  \ V /
+// \_____|  \___|  \__|   |_____| |_| |_|  \__,_| |_|   \_/
+// */
+
+// GET incividual book details
+//------> Find by id to retrieve specific book detail
+router.get('/:id', function(req, res, next) {
+  Book.findById(req.params.id).then(function(bookDetail){
+    res.render('book_detail',{bookDetail: bookDetail});
+  }).catch(function(error){
+      res.send(500, error);
+   });
+});
 // // router.get('/:id', function(req, res, next) {
 // //   Book.findAll({
 // //     include: [{ model: Loan, include: [{ model: Patron }] }],
--- routes/index.js
+++ routes/index.js
@@ -12,98 +12,98 @@ router.get('/', function(req, res, next) {
 });
 
 
-//BOOKS
-//BOOKS
-//BOOKS
-/* _____          _
- / ____|        | |
-| |  __    ___  | |_
-| | |_ |  / _ \ | __|
-| |__| | |  __/ | |_
-\_____|  \___|  \__|
-*/
-//* GET all Books listings. */
-router.get('/books', function(req, res, next) {
-  Book.findAll().then(function(books){
-    res.render('books',{books: books});
-  }).catch(function(error){
-      res.send(500, error);
-   });
-});
-
-router.get('/new_book', function(req, res, next) {
-  res.render('new_book');
-});
-router.get('/overdue_books', function(req, res, next) {
-  res.render('overdue_books');
-});
-router.get('/checked_books', function(req, res, next) {
-  res.render('checked_books');
-});
-router.get('/book_detail', function(req, res, next) {
-  // console.log("one");
-  res.render('book_detail');
-});
-
-/*
-  _____                  _
- |  __ \                | |
- | |__) |   ___    ___  | |_
- |  ___/   / _ \  / __| | __|
- | |      | (_) | \__ \ | |_
- |_|      \___/  |___/  \__|
-
-*//* POST create article. */
-router.post('/new_book', function(req, res, next) {
-  Book.create(req.body).then(function(book) {
-    res.redirect("/books");
-  }).catch(function(error){
-      if(error.name === "SequelizeValidationError") {
-        console.log("error");
-      } else {
-        throw error;
-      }
-  }).catch(function(error){
-      res.send(500, error);
-   });
-;});
-/*
- _____            _
-|  __ \          | |
-| |__) |  _   _  | |_
-|  ___/  | | | | | __|
-| |      | |_| | | |_
-|_|       \__,_|  \__|
-*/
-router.put('/book_detail/:id', function(req, res, next){
-  Book.findById(req.params.id).then(function(bookDetail){
-    return bookDetail.update(req.body);
-  }).then(function(bookDetail){
-    res.redirect('/book_detail/' + bookDetail.id);
-  }).catch(function(error){
-      console.log("there is a huge 500 error here");
-      res.status(500).send(error);
-   });
-});
-
-// /*
-//  _____          _       _____               _   _
-// / ____|        | |     |_   _|             | | (_)
-// | |  __    ___  | |_      | |    _ __     __| |  _  __   __
-// | | |_ |  / _ \ | __|     | |   | '_ \   / _` | | | \ \ / /
-// | |__| | |  __/ | |_     _| |_  | | | | | (_| | | |  \ V /
-// \_____|  \___|  \__|   |_____| |_| |_|  \__,_| |_|   \_/
+// //BOOKS
+// //BOOKS
+// //BOOKS
+// /* _____          _
+//  / ____|        | |
+// | |  __    ___  | |_
+// | | |_ |  / _ \ | __|
+// | |__| | |  __/ | |_
+// \_____|  \___|  \__|
 // */
-
-// GET incividual book details
-//------> Find by id to retrieve specific book detail
-router.get('/book_detail/:id', function(req, res, next) {
-  Book.findById(req.params.id).then(function(bookDetail){
-    res.render('book_detail',{bookDetail: bookDetail});
-  }).catch(function(error){
-      res.send(500, error);
-   });
-});
+// //* GET all Books listings. */
+// router.get('/books', function(req, res, next) {
+//   Book.findAll().then(function(books){
+//     res.render('books',{books: books});
+//   }).catch(function(error){
+//       res.send(500, error);
+//    });
+// });
+//
+// router.get('/new_book', function(req, res, next) {
+//   res.render('new_book');
+// });
+// router.get('/overdue_books', function(req, res, next) {
+//   res.render('overdue_books');
+// });
+// router.get('/checked_books', function(req, res, next) {
+//   res.render('checked_books');
+// });
+// router.get('/book_detail', function(req, res, next) {
+//   // console.log("one");
+//   res.render('book_detail');
+// });
+//
+// /*
+//   _____                  _
+//  |  __ \                | |
+//  | |__) |   ___    ___  | |_
+//  |  ___/   / _ \  / __| | __|
+//  | |      | (_) | \__ \ | |_
+//  |_|      \___/  |___/  \__|
+//
+// *//* POST create article. */
+// router.post('/new_book', function(req, res, next) {
+//   Book.create(req.body).then(function(book) {
+//     res.redirect("/books");
+//   }).catch(function(error){
+//       if(error.name === "SequelizeValidationError") {
+//         console.log("error");
+//       } else {
+//         throw error;
+//       }
+//   }).catch(function(error){
+//       res.send(500, error);
+//    });
+// ;});
+// /*
+//  _____            _
+// |  __ \          | |
+// | |__) |  _   _  | |_
+// |  ___/  | | | | | __|
+// | |      | |_| | | |_
+// |_|       \__,_|  \__|
+// */
+// router.put('/book_detail/:id', function(req, res, next){
+//   Book.findById(req.params.id).then(function(bookDetail){
+//     return bookDetail.update(req.body);
+//   }).then(function(bookDetail){
+//     res.redirect('/book_detail/' + bookDetail.id);
+//   }).catch(function(error){
+//       console.log("there is a huge 500 error here");
+//       res.status(500).send(error);
+//    });
+// });
+//
+// // /*
+// //  _____          _       _____               _   _
+// // / ____|        | |     |_   _|             | | (_)
+// // | |  __    ___  | |_      | |    _ __     __| |  _  __   __
+// // | | |_ |  / _ \ | __|     | |   | '_ \   / _` | | | \ \ / /
+// // | |__| | |  __/ | |_     _| |_  | | | | | (_| | | |  \ V /
+// // \_____|  \___|  \__|   |_____| |_| |_|  \__,_| |_|   \_/
+// // */
+//
+// // GET incividual book details
+// //------> Find by id to retrieve specific book detail
+// router.get('/book_detail/:id', function(req, res, next) {
+//   Book.findById(req.params.id).then(function(bookDetail){
+//     res.render('book_detail',{bookDetail: bookDetail});
+//   }).catch(function(error){
+//       res.send(500, error);
+//    });
+// });
 // //PATRONS
 // //PATRONS
 // //PATRONS
