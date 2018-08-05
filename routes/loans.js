7a13bc761851bffd95c80c7257961040a5a21286
darryldriedger <darryldriedger@gmail.com>
2018-08-05 01:12:48 -0600

    added data generated from database to the new loan dropdowns

--- routes/loans.js
+++ routes/loans.js
@@ -17,15 +17,38 @@ router.get('/', function(req, res, next) {
 router.get('/checked_loans', function(req, res, next) {
   res.render('loans/checked_loans');
 });
-router.get('/new_loan', function(req, res, next) {
-  Book.findAll().then(function(books){
-    res.render('loans/new_loan',{books: books});
-  }).catch(function(error){
-      res.send(500, error);
-   });
-});
+// router.get('/new_loan', function(req, res, next) {
+//   Book.findAll().then(function(books){
+//     res.render('loans/new_loan',{books: books});
+//   }).catch(function(error){
+//       res.send(500, error);
+//    });
+// });
 router.get('/overdue_loans', function(req, res, next) {
   res.render('loans/overdue_loans');
 });
+
+
+
+router.get('/new_loan', function(req, res, next) {
+  var books;
+  var patrons;
+
+  Book.findAll().then(function(results){
+    books = results;
+  }).then(
+    Patron.findAll().then(function(results){
+      patrons = results;
+    }).then(function(){
+      res.render('loans/new_loan', {
+        books: books,
+        patrons: patrons});
+    }).catch(function(err){
+      return next(err);
+    })
+  );
+});
+
+
 
 module.exports = router;
