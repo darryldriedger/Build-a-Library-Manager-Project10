e02a2918537e3a524b1894bd20ac18602142233c
darryldriedger <darryldriedger@gmail.com>
2018-08-06 23:47:50 -0600

    changes to books nav and routing; generating all books info to overdue and checked out; working

--- routes/books.js
+++ routes/books.js
@@ -9,20 +9,30 @@ var Patron = require("../models").Patron;
 //* GET all Books listings. GET GET GET GET GET */
 router.get('/', function(req, res, next) {
   Book.findAll().then(function(books){
-    res.render('books',{books: books});
+    res.render('books',{books: books,title: 'Books'});
   }).catch(function(error){
       res.send(500, error);
    });
 });
 
 router.get('/new_book', function(req, res, next) {
-  res.render('books/new_book');
+  res.render('books/new_book',{title: 'New Book'});
 });
+
 router.get('/overdue_books', function(req, res, next) {
-  res.render('books/overdue_books');
+  Book.findAll().then(function(books){
+    res.render('books/overdue_books',{books: books,title: 'Overdue Books'});
+  }).catch(function(error){
+      res.send(500, error);
+   });
 });
+
 router.get('/checked_books', function(req, res, next) {
-  res.render('books/checked_books');
+  Book.findAll().then(function(books){
+    res.render('books/checked_books',{books: books,title: 'Checked Out Books'});
+  }).catch(function(error){
+      res.send(500, error);
+   });
 });
 
 /* POST create article. POST POST POST POST POST */
