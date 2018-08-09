606529985366f7adf630493b834f8f6eafdde716
darryldriedger <darryldriedger@gmail.com>
2018-08-08 23:11:29 -0600

    changes to validation message in book model

--- models/book.js
+++ models/book.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       validate: {
         notEmpty: {
-          msg: "Please enter a title"
+          msg: "Title is required"
         }
       }
     },
